3df1f33b8130db714e1aac0cbf16676265af51fa
codjoe4@gmail.com <codjoe4@gmail.com>
2019-02-23 14:52:01 -0500

    * Contact.cshtml: New testing couchDB
    * Notification.cs:
    * HomeControler.cs:
    * socialconnectDATA-API.csproj:
    * NotificationsController.cs:

---
+++ socialconnectDATA-API/Models/Notification.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace socialconnectDATAAPI.Models
+{
+    public class Notification
+    {
+       public string Alert { get; set; }
+        public int Count { get; set; }
+    }
+}
